3320aaf087ca31e1c8ae3c0e9252764faee90bf6
antmachine <anthonysallows@gmail.com>
2014-08-09 13:48:20 -0700

    View to posts index comes up

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+
+def index
+	@post = Post.all
+end
+
+def create
+	
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  root 'post#index'
+  root 'posts#index'
 
 end
