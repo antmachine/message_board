f0d3e1e050b9066048a29cfcfeb91fdac32d1150
antmachine <anthonysallows@gmail.com>
2014-08-12 18:57:21 -0700

    Added Name and Title columns to post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140809203543) do
+ActiveRecord::Schema.define(version: 20140813015637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20140809203543) do
     t.string   "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "title"
   end
 
   create_table "users", force: true do |t|
